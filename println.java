d6845d8514ac991fb75ad8ab751e87c4e2962bd4
QDCore <www.sungrins4@gmail.com>
2022-05-31 17:33:06 +0900

    정리

--- println.java
+++
@@ -1,9 +0,0 @@
-public class println{
-        public static void main(String[] args) {
-                for(int i = 2; i < 9; i++){
-                    for(int a = 2; a < 9; a++){
-                        System.out.printf("%d X %d = %d\n", i, a, a * i);
-                    }
-                }    
-        }
-    }
